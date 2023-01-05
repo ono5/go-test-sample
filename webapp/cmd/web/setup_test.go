41c9d628fa03572cebc4853c6fcc2678b4ab7e1b
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-01-05 18:24:17 +0900

    add repository test

--- webapp/cmd/web/setup_test.go
+++ webapp/cmd/web/setup_test.go
@@ -1,25 +1,18 @@
 package main
 
 import (
-	"log"
 	"os"
 	"testing"
-	"webapp/pkg/db"
+	"webapp/pkg/repository/dbrepo"
 )
 
 var app application
 
 func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
+
 	app.Session = getSession()
-	app.DSN = "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5"
+	app.DB = &dbrepo.TestDBRepo{}
 
-	conn, err := app.connectToDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	app.DB = db.PostgresConn{DB: conn}
 	os.Exit(m.Run())
 }
