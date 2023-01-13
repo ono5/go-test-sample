fd50b54888bf2616308d4a8d05b1f16986b29907
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-01-14 05:33:43 +0900

    add token generator

--- webapp/cmd/api/setup_test.go
+++ webapp/cmd/api/setup_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 var app application
+var expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiYXVkIjoiZXhhbXBsZS5jb20iLCJleHAiOjE2NzMyODE5NjYsImlzcyI6ImV4YW1wbGUuY29tIiwibmFtZSI6IkpvaG4gRG9lIiwic3ViIjoiMSJ9.8iAXTDCh9jw9xpYwCguYL9NjEOIuvs9ZWsneDzJMTBs"
 
 func TestMain(m *testing.M) {
 	app.DB = &dbrepo.TestDBRepo{}
