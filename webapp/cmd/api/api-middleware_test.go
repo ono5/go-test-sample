e38ce25834421b99404e99e9f4c666a6d5eaf3e0
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-01-15 07:25:52 +0900

    add middleware and test

---
+++ webapp/cmd/api/api-middleware_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_app_enableCORS(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	var tests = []struct {
+		name           string
+		method         string
+		expectedHeader bool
+	}{
+		{"preflight", "OPTIONS", true},
+		{"get", "GET", false},
+	}
+
+	for _, e := range tests {
+		handlerToTest := app.enableCORS(nextHandler)
+
+		req := httptest.NewRequest(e.method, "http://testing", nil)
+		rr := httptest.NewRecorder()
+
+		handlerToTest.ServeHTTP(rr, req)
+
+		if e.expectedHeader && rr.HeaderMap.Get("Access-Control-Allow-Credentials") == "" {
+			t.Errorf("%s: expected header, but did not find it", e.name)
+		}
+
+		if !e.expectedHeader && rr.Header().Get("Access-Control-Allow-Credentials") != "" {
+			t.Errorf("%s: expected no header, but got one", e.name)
+		}
+	}
+}
