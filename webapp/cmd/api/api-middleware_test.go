f477253d6732572f1ce8e622478e28041e008b92
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-01-15 07:37:59 +0900

    add auth test2

--- webapp/cmd/api/api-middleware_test.go
+++ webapp/cmd/api/api-middleware_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"webapp/pkg/data"
 )
 
 func Test_app_enableCORS(t *testing.T) {
@@ -32,6 +34,49 @@ func Test_app_enableCORS(t *testing.T) {
 
 		if !e.expectedHeader && rr.Header().Get("Access-Control-Allow-Credentials") != "" {
 			t.Errorf("%s: expected no header, but got one", e.name)
+		}
+	}
+}
+
+func Test_app_authRequired(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	testUser := data.User{
+		ID:        1,
+		FirstName: "Admin",
+		LastName:  "User",
+		Email:     "admin@example.com",
+	}
+
+	tokens, _ := app.generateTokenPair(&testUser)
+
+	var tests = []struct {
+		name            string
+		token           string
+		expectAutorized bool
+		setHeader       bool
+	}{
+		{name: "valid token", token: fmt.Sprintf("Bearer %s", tokens.Token), expectAutorized: true, setHeader: true},
+		{name: "no token", token: "", expectAutorized: false, setHeader: false},
+		{name: "invalid token", token: fmt.Sprintf("Bearer %s", expiredToken), expectAutorized: false, setHeader: true},
+	}
+
+	for _, e := range tests {
+		req, _ := http.NewRequest("GET", "/", nil)
+		if e.setHeader {
+			req.Header.Set("Authorization", e.token)
+		}
+		rr := httptest.NewRecorder()
+
+		handlerToTest := app.authRequired(nextHandler)
+		handlerToTest.ServeHTTP(rr, req)
+
+		if e.expectAutorized && rr.Code == http.StatusUnauthorized {
+			t.Errorf("%s: got code 401, and should not have", e.name)
+		}
+
+		if !e.expectAutorized && rr.Code != http.StatusUnauthorized {
+			t.Errorf("%s: did not get code 401, and should have", e.name)
 		}
 	}
 }
