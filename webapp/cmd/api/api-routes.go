e5c96052c23077b21830d3a9c05fc8023cfe501a
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-01-18 17:59:47 +0900

    add spa html

--- webapp/cmd/api/api-routes.go
+++ webapp/cmd/api/api-routes.go
@@ -15,6 +15,8 @@ func (app *application) routes() http.Handler {
 
 	// mux.Use(app.enableCORS)
 
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./html/"))))
+
 	// authentication routes - auth handler, refresh
 	mux.Post("/auth", app.authenticate)
 	mux.Post("/refresh-token", app.refresh)
