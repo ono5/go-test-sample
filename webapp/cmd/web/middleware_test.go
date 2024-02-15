79e400f3d6b660c12bd6251bfb6e9cbcb7eaa4d5
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-02-16 05:55:32 +0900

    set up a test env with testing.M

--- webapp/cmd/web/middleware_test.go
+++ webapp/cmd/web/middleware_test.go
@@ -8,8 +8,6 @@ import (
 	"testing"
 )
 
-var app application
-
 func Test_application_addIPToContext(t *testing.T) {
 	tests := []struct {
 		headerName  string
@@ -62,9 +60,6 @@ func Test_application_addIPToContext(t *testing.T) {
 }
 
 func Test_application_ipFromContext(t *testing.T) {
-	// create an app var of type application
-	var app application
-
 	// get a context
 	ctx := context.Background()
 
