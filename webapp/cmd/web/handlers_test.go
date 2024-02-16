675470438aca3135a5ee67f495c548c0beadbc29
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-02-17 06:57:53 +0900

    improv our test for the home handler

--- webapp/cmd/web/handlers_test.go
+++ webapp/cmd/web/handlers_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -42,7 +43,7 @@ func Test_application_handlers(t *testing.T) {
 	}
 }
 
-func TestAppHome(t *testing.T) {
+func TestAppHomeOld(t *testing.T) {
 	// create a request
 	req, _ := http.NewRequest("GET", "/", nil)
 
@@ -60,8 +61,48 @@ func TestAppHome(t *testing.T) {
 	}
 
 	body, _ := io.ReadAll(rr.Body)
-	if !strings.Contains(string(body), `<small>From Sessoin:`) {
+	if !strings.Contains(string(body), `<small>From Session:`) {
 		t.Error("did not find correct text in html")
+	}
+}
+
+func TestAppHome(t *testing.T) {
+	var tests = []struct {
+		name         string
+		putInSession string
+		expectedHTML string
+	}{
+		{"first visit", "", "<small>From Session:"},
+		{"second visit", "hello, world!", "<small>From Session: hello, world!"},
+	}
+
+	for _, e := range tests {
+		// create a request
+		req, _ := http.NewRequest("GET", "/", nil)
+
+		req = addContextAndSessionToRequest(req, app)
+		_ = app.Session.Destroy(req.Context())
+
+		if e.putInSession != "" {
+			app.Session.Put(req.Context(), "test", e.putInSession)
+		}
+
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(app.Home)
+
+		handler.ServeHTTP(rr, req)
+
+		// check status code
+		if rr.Code != http.StatusOK {
+			t.Errorf("TestAppHome returend wrong status code; expected 200 but got %d", rr.Code)
+		}
+
+		body, _ := io.ReadAll(rr.Body)
+		fmt.Println(string(body))
+		if !strings.Contains(string(body), e.expectedHTML) {
+			t.Errorf("%s: did not find %s in response body", e.name, e.expectedHTML)
+		}
 	}
 }
 
